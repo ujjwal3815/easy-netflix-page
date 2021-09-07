6a9abbab7d86688845ca415bacdd1b99bcfa2637
ujjwal3815 <ujjwaltiwari201820@gmail.com>
2021-09-08 02:04:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-8x51hg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-8x51hg)
---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './style.css';
+
+const App = () => {
+  return (
+   <>
+   <h1> Ujjwal Tiwari Netflix Picks  </h1>
+   <h2> List of 5 Best Series :  </h2>
+   <ol>
+     <li> Stranger Things </li>
+     <li> Dark </li>
+     <li> Mr. ROBOT </li>
+     <li> Extra Curricular </li>
+     <li> My First 2 Love </li>
+  </ol>
+
+
+   </>
+  )
+}
+
+
+export default App
